4992bf1c832211dc9592e2060041ef4b8558e0f9
Iván Zaera Avellón <izaera@gmail.com>
2017-11-22 14:07:05 +0100

    Make version check in foreground to minimize the chances of outdated reports

--- bin/lnka.js
+++ bin/lnka.js
@@ -3,48 +3,89 @@ const yargs = require('yargs');
 const cmd = require('../lib/index').default;
 
 // Show notice when a new version is ready
-const updateNotifier = require('update-notifier');
-const pkg = require('../package.json');
-updateNotifier({pkg}).notify();
+let latestVersion = require('latest-version');
+let semver = require('semver');
+let pkg = require('../package.json');
 
-// Define command line usage and launch correct handler
-yargs
-  .command({
-    command: 'features',
-    desc: 'Analyze project and show feature level',
-    handler: cmd.features,
-    builder: {
-      server: {
-        describe: 'Server address',
-        default: 'localhost',
-      },
-      httpPort: {
-        describe: 'HTTP port',
-        default: '8080',
-      },
-      gogoPort: {
-        describe: 'GoGo shell port',
-        default: '11311',
-      },
-    },
+latestVersion(pkg.name)
+  .then(version => {
+    if (semver.lt(pkg.version, version)) {
+      console.log(
+        '\n',
+        '#\n',
+        '#                     W  A  R  N  I  N  G\n',
+        '#\n',
+        '# There is a new version of lnka',
+        '(' + version + '). Please update it running:\n',
+        '#\n',
+        '#       npm update -g liferay-npm-sdk-assistant\n',
+        '#\n',
+        '# Otherwise you may get outdated info from this tool.\n',
+        '#\n'
+      );
+    }
+
+    main();
   })
-  .option('debug', {
-    describe: 'Show debug information about what is being done',
-    default: false,
-  })
-  .command({
-    command: 'man',
-    desc: 'Show information or documentation about the SDK',
-    handler: cmd.man,
-    builder: yargs =>
-      yargs
-        .command({
-          command: 'features',
-          desc: 'List feature levels and minimum component versions',
-          handler: cmd.man,
-        })
-        .demandCommand(1, 'Doing nothing: no item to describe provided\n'),
-  })
-  .demandCommand(1, 'Doing nothing: no command specified\n')
-  .help()
-  .wrap(80).argv;
+  .catch(err => {
+    console.log(
+      '\n',
+      '#\n',
+      '#                   W  A  R  N  I  N  G\n',
+      '#\n',
+      '# Could not check if there is a newer version of lnka.\n',
+      '#\n',
+      '# This means that you may get outdated info from this tool if\n',
+      '# a new version has been released and you are not up-to-date.\n',
+      '#\n'
+    );
+
+    main();
+  });
+
+/**
+ * Main entry point
+ * @return {void}
+ */
+function main() {
+  yargs
+    .command({
+      command: 'features',
+      desc: 'Analyze project and show feature level',
+      handler: cmd.features,
+      builder: {
+        server: {
+          describe: 'Server address',
+          default: 'localhost',
+        },
+        httpPort: {
+          describe: 'HTTP port',
+          default: '8080',
+        },
+        gogoPort: {
+          describe: 'GoGo shell port',
+          default: '11311',
+        },
+      },
+    })
+    .option('debug', {
+      describe: 'Show debug information about what is being done',
+      default: false,
+    })
+    .command({
+      command: 'man',
+      desc: 'Show information or documentation about the SDK',
+      handler: cmd.man,
+      builder: yargs =>
+        yargs
+          .command({
+            command: 'features',
+            desc: 'List feature levels and minimum component versions',
+            handler: cmd.man,
+          })
+          .demandCommand(1, 'Doing nothing: no item to describe provided\n'),
+    })
+    .demandCommand(1, 'Doing nothing: no command specified\n')
+    .help()
+    .wrap(80).argv;
+}
