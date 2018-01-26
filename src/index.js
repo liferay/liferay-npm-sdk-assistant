28e7a2755493e85d6236aa52dabc4f2e0bb97a25
Iván Zaera Avellón <izaera@gmail.com>
2018-01-25 17:11:09 -0800

    Add new resolve command
    Fixes #10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import features from './cmd/features';
 import man from './cmd/man';
+import resolve from './cmd/resolve';
 
 export default {
   features: features,
   man: man,
+  resolve: resolve,
 };
